130cf7b42dfaee891682077a2938e3230a2735b1
AR-Raqeeb <arraqeeb628@gmail.com>
2021-08-07 14:59:30 +0600

    'newFeaturesAdded'

--- jarvis.py
+++ jarvis.py
@@ -1,77 +1,157 @@
 import speech_recognition as sr # this package will recognize the voice command
 import pyttsx3 # this package will convert text-to-speech
 import pywhatkit # this package can search and play videos on youtube following the voice command
-import datetime
-import wikipedia
-import requests, json , sys
-import pyjokes
+import datetime     # to know the current date and time
+import wikipedia    # to explore wikipedia entries
+import requests     # a library for making HTTP requests
+import time
+import webbrowser   # open any domain on the internet using browser
 
-listener = sr.Recognizer() # creating a speech recognizer to recognize the voice of the user
-engine = pyttsx3.init() # initializing the text-to-speech engine
+
+print('Loading your AI personal assistant - Jarvis')
+
+engine=pyttsx3.init('sapi5')    # initializing the text-to-speech engine
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-def weather(city): #weather used openweathermap api take city name as input
-    # API key
-    api_key = "e109b74f2efd786c4194d0efb32c9211"
-    # base_url variable to store url
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    # city name
-    city_name = city
-
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-    response = requests.get(complete_url)
-    x = response.json()
-    if x["cod"] != "404":
-        y = x["main"]
-        current_temperature = y["temp"]
-        return str(current_temperature)
-
-def take_command():
-    try:
-        with sr.Microphone() as source: # declaring the Microphone as source
-            print('Say Something...') # user can start giving command when they see this line
-            voice = listener.listen(source) # calling the speech recognizer to listen to the source
-            command = listener.recognize_google(voice) # using google API to convert the audio into text
-            command = command.lower() # keeping the command in lowercase
-        if 'jarvis' in command: # this block will run if the command contains the word "jarvis"
-            command = command.replace('jarvis', '') # we are replacing the word jarvis with an empty string so that it doesn't show up in the command
-            talk(command) # jarvis will say out loud the text we got from google API converter
-
-    except: # if the command doesn't include the word "jarvis", then this block will run
-        pass # we are going to ignore this block
-    return command
-
-def run_jarvis():
-    command = take_command()
-    if 'play' in command:
-        ytvideo = command.replace('play', '')
-        talk('playing' + ytvideo)
-        pywhatkit.playonyt(ytvideo)
-
-    if 'time' in command: #speak date & time
-            time  = datetime.datetime.now().strftime('%I:%M %p')
-            print(time)
-            talk('Current time is ' + time)
-
-    elif 'tell me about' in command: #read from wikipedia used wikipedia library
-            look_for = command.replace('tell me about', '')
-            info = wikipedia.summary(look_for, 1)
-            print(info)
-            talk(info)
-
-    elif 'weather' in command: #weather In location
-            weather_api = weather('Sylhet') #pass city name in weather api function
-            talk(weather_api + 'degree fahreneit')
-
-    elif 'joke' in command:  # tell jokes used pyjokes library
-            talk(pyjokes.get_joke())
-
+def wishMe():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        talk("Hello,Good Morning")
+        print("Hello,Good Morning")
+    elif hour>=12 and hour<18:
+        talk("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
     else:
-        talk('I did not get it but I am going to search it for you') #search at google used pywhatkit library
-        pywhatkit.search(command)
+        talk("Hello,Good Evening")
+        print("Hello,Good Evening")
 
-while True:     #run alexa continuously -> take command from user one after another
-    run_jarvis()
+def take_Command():
+    r = sr.Recognizer()     # creating a speech recognizer to recognize the voice of the user
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+        try:
+            statement = r.recognize_google(audio, language='en-in')
+            print(f"Command: {statement}\n")
+
+        except Exception as e:
+            talk("Pardon me, please say that again")
+            return "None"
+        return statement
+
+talk("Loading your AI personal assistant Jarvis")
+wishMe()
+
+
+if __name__=='__main__':
+
+
+    while True:
+        talk("Tell me how can I help you now?")
+        statement = take_Command().lower()
+        if statement==0:
+            continue
+
+        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+            talk('your personal assistant Jarvis is shutting down,Good bye')
+            print('your personal assistant Jarvis is shutting down,Good bye')
+            break
+
+
+
+        elif 'wikipedia' in statement:
+            talk('Searching Wikipedia...')
+            statement =statement.replace("wikipedia", "")
+            results = wikipedia.summary(statement, sentences=3)
+            talk("According to Wikipedia")
+            print(results)
+            talk(results)
+
+        elif 'play' in statement:
+            ytvideo = statement.replace('play', '')
+            talk('playing' + ytvideo)
+            pywhatkit.playonyt(ytvideo)
+
+        elif 'open youtube' in statement:
+            webbrowser.open_new_tab("https://www.youtube.com")
+            talk("youtube is open now")
+            time.sleep(5)
+
+        elif 'open google' in statement:
+            webbrowser.open_new_tab("https://www.google.com")
+            talk("Google chrome is open now")
+            time.sleep(5)
+
+        elif 'open gmail' in statement:
+            webbrowser.open_new_tab("gmail.com")
+            talk("Google Mail open now")
+            time.sleep(5)
+
+        elif "weather" in statement:
+            api_key="8ef61edcf1c576d65d836254e11ea420"
+            base_url="https://api.openweathermap.org/data/2.5/weather?"
+            talk("whats the city name")
+            city_name=take_Command()
+            complete_url=base_url+"appid="+api_key+"&q="+city_name
+            response = requests.get(complete_url)
+            x = response.json()
+            if x["cod"]!="404":
+                y=x["main"]
+                current_temperature = y["temp"]
+                current_humidiy = y["humidity"]
+                z = x["weather"]
+                weather_description = z[0]["description"]
+                talk(" Temperature in kelvin unit is " +
+                      str(current_temperature) +
+                      "\n humidity in percentage is " +
+                      str(current_humidiy) +
+                      "\n description  " +
+                      str(weather_description))
+                print(" Temperature in kelvin unit = " +
+                      str(current_temperature) +
+                      "\n humidity (in percentage) = " +
+                      str(current_humidiy) +
+                      "\n description = " +
+                      str(weather_description))
+
+            else:
+                talk(" City Not Found ")
+
+
+
+        elif 'time' in statement:
+            strTime=datetime.datetime.now().strftime("%H:%M:%S")
+            talk(f"the time is {strTime}")
+
+        elif 'who are you' in statement or 'what can you do' in statement:
+            talk('I am Jarvis, version 1 point O, your persoanl assistant. I am programmed to minor tasks like'
+                  'opening youtube, google chrome, gmail and stackoverflow , tell time, search wikipedia entries, tell weather' 
+                  'in different cities , get top world news from the CNN and play any videos from youtube too!')
+
+
+        elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
+            talk("I was built by Raqeeb and Tahmid")
+            print("I was built by Raqeeb and Tahmid")
+
+        elif "open stack overflow" in statement:
+            webbrowser.open_new_tab("https://stackoverflow.com")
+            talk("Here is stack overflow")
+
+        elif 'news' in statement:
+            news = webbrowser.open_new_tab("https://edition.cnn.com/world")
+            talk('Here are some headlines from the CNN,Happy reading')
+            time.sleep(10)
+
+        elif 'search'  in statement:
+            statement = statement.replace("search", "")
+            webbrowser.open_new_tab(statement)
+            time.sleep(6)
+
+
+time.sleep(3)
