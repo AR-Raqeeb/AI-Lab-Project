cf02b047cf7b48f8c964eb7b06f065a973ff38c0
AR-Raqeeb <arraqeeb628@gmail.com>
2021-08-16 21:58:28 +0600

    'FinalSubmit'

--- jarvis.py
+++ jarvis.py
@@ -19,7 +19,7 @@ def talk(text):
     engine.runAndWait()
 
 def wishMe():
-    hour=datetime.datetime.now().hour
+    hour=datetime.datetime.now().hour   # this is to wish user according to time
     if hour>=0 and hour<12:
         talk("Hello,Good Morning")
         print("Hello,Good Morning")
@@ -58,14 +58,14 @@ if __name__=='__main__':
         if statement==0:
             continue
 
-        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+        if "good bye" in statement or "ok bye" in statement or "stop" in statement:     # good bye means it will shut down
             talk('your personal assistant Jarvis is shutting down,Good bye')
             print('your personal assistant Jarvis is shutting down,Good bye')
             break
 
 
 
-        elif 'wikipedia' in statement:
+        elif 'wikipedia' in statement:      # search on wikipedia, print and speak the results
             talk('Searching Wikipedia...')
             statement =statement.replace("wikipedia", "")
             results = wikipedia.summary(statement, sentences=3)
@@ -73,35 +73,35 @@ if __name__=='__main__':
             print(results)
             talk(results)
 
-        elif 'play' in statement:
+        elif 'play' in statement:       # play anything from youtube according to command
             ytvideo = statement.replace('play', '')
             talk('playing' + ytvideo)
             pywhatkit.playonyt(ytvideo)
 
-        elif 'open youtube' in statement:
+        elif 'open youtube' in statement:       # open youtube
             webbrowser.open_new_tab("https://www.youtube.com")
             talk("youtube is open now")
             time.sleep(5)
 
-        elif 'open google' in statement:
+        elif 'open google' in statement:        # open google
             webbrowser.open_new_tab("https://www.google.com")
             talk("Google chrome is open now")
             time.sleep(5)
 
-        elif 'open gmail' in statement:
+        elif 'open gmail' in statement:     # open gmail
             webbrowser.open_new_tab("gmail.com")
             talk("Google Mail open now")
             time.sleep(5)
 
-        elif "weather" in statement:
+        elif "weather" in statement:        # weather of any location
             api_key="8ef61edcf1c576d65d836254e11ea420"
             base_url="https://api.openweathermap.org/data/2.5/weather?"
             talk("whats the city name")
-            city_name=take_Command()
+            city_name = take_Command()
             complete_url=base_url+"appid="+api_key+"&q="+city_name
             response = requests.get(complete_url)
             x = response.json()
-            if x["cod"]!="404":
+            if x["cod"] != "404":
                 y=x["main"]
                 current_temperature = y["temp"]
                 current_humidiy = y["humidity"]
@@ -125,30 +125,30 @@ if __name__=='__main__':
 
 
 
-        elif 'time' in statement:
+        elif 'time' in statement:       # Time show
             strTime=datetime.datetime.now().strftime("%H:%M:%S")
             talk(f"the time is {strTime}")
 
-        elif 'who are you' in statement or 'what can you do' in statement:
-            talk('I am Jarvis, version 1 point O, your persoanl assistant. I am programmed to minor tasks like'
+        elif 'who are you' in statement or 'what can you do' in statement:      # explains what it is and what it can do
+            talk('I am Jarvis, version 1 point O, your personal assistant. I am programmed to minor tasks like'
                   'opening youtube, google chrome, gmail and stackoverflow , tell time, search wikipedia entries, tell weather' 
                   'in different cities , get top world news from the CNN and play any videos from youtube too!')
 
 
-        elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
+        elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:        # name of who made it
             talk("I was built by Raqeeb and Tahmid")
             print("I was built by Raqeeb and Tahmid")
 
-        elif "open stack overflow" in statement:
+        elif "open stack overflow" in statement:        # open stack overflow
             webbrowser.open_new_tab("https://stackoverflow.com")
             talk("Here is stack overflow")
 
-        elif 'news' in statement:
+        elif 'news' in statement:       # open cnn to read news
             news = webbrowser.open_new_tab("https://edition.cnn.com/world")
             talk('Here are some headlines from the CNN,Happy reading')
             time.sleep(10)
 
-        elif 'search'  in statement:
+        elif 'search' in statement:        # search anything on google
             statement = statement.replace("search", "")
             webbrowser.open_new_tab(statement)
             time.sleep(6)
